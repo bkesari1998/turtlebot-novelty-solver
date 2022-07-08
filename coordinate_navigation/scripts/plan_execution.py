32335830c3ad81cbae69f7d884c104e0e3bb8671
Bharat Kesari <bkesari1998@gmail.com>
2022-07-08 19:08:56 -0400

    added soundplay launch files

---
+++ coordinate_navigation/scripts/plan_execution.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+from waypoints_dict import waypoints
+
+import rospy
+import actionlib
+from actionlib_msgs.msg import GoalStatus
+
+from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
+from geometry_msgs.msg import Twist
+from std_srvs.srv import Trigger
+
+class PlanExecutor():
+    
+    def __init__(self):
+
+        # Initialize node
+        rospy.init_node("move_to_start", anonymous=False)
+        rospy.loginfo("Moving turtlebot to starting position")
+        rospy.on_shutdown(self.shutdown)
+
+        self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10) # publisher of Twist msg to robot
+        self.rate = rospy.Rate(1) # publish at 1 Hz
+        
+        rospy.wait_for_service("move_to_start")
+        rospy.wait_for_service("dock")
+
+        try:
+            move_to_start = rospy.ServiceProxy("move_to_start", Trigger)
+            response = move_to_start()
+        except rospy.ServiceException as e:
+            rospy.logerr(e)
+        
+        try:
+            dock = rospy.ServiceProxy("dock", Trigger)
+            response = dock()
+        except rospy.ServiceException as e:
+            rospy.logerr(e)
+    
+    def shutdown(self):
+
+        rospy.loginfo("Stopping plan_executor node")
+        
+        # Stop the turtlebot
+        self.cmd_vel.publish(Twist())
+        rospy.sleep()
+
+if __name__ == "__main__":
+    
+    try:
+        PlanExecutor()
+    except:
+        rospy.logerr("PlanExecutor failed")
