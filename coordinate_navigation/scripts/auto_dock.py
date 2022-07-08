c8375d4eab5bee06ae93e38907ba280f39a07096
Bharat Kesari <bkesari1998@gmail.com>
2022-07-08 15:30:58 -0400

    updated auto_dock.py with os function

--- coordinate_navigation/scripts/auto_dock.py
+++ coordinate_navigation/scripts/auto_dock.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python
 
 import rospy
-import roslaunch
+import os
 
 from std_msgs.msg import Bool
 
 def dock(msg):
-    node = roslaunch.core.Node("kobuki_auto_docking", "DockDriveActionClient.py")
-    launch = roslaunch.scriptapi.ROSLaunch()
-    launch.start()
-    launch.launch(node)
+    os.system("roslaunch kobuki_auto_docking activate.launch")
 
 if __name__ == "__main__":
 
