30b9844fbda4f2116c28e16e2c40ce7c66ad4334
Bharat Kesari <bkesari1998@gmail.com>
2022-07-13 12:23:13 -0400

    added shutdown function and comments

--- coordinate_navigation/scripts/move_to_location.py
+++ coordinate_navigation/scripts/move_to_location.py
@@ -6,13 +6,25 @@ import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
 from actionlib_msgs.msg import GoalStatus
 from coffee_bot_srvs.srv import Move
+from geometry_msgs.msg import Twist
 
 class MoveTB():
     def __init__(self):
+        '''
+        Initializes move_tb ROS node.
+        '''
 
+        # Initialize node
         rospy.init_node("move_tb", anonymous=False)
+        rospy.loginfo("move_tb node active")
+        rospy.on_shutdown(self.shutdown)
 
+        # Velocity command publisher for shutdown
+        self.cmd_vel = rospy.Publisher('mobile_base/commands/velocity', Twist, queue_size=10)
+
+        # Initialize service
         self.move_tb_srv = rospy.Service("/move", Move, self.move_tb)
+        rospy.loginfo("move service active")
 
         # Create a SimpleActionClient of a move_base action type and wait for server
         self.simple_action_client = actionlib.SimpleActionClient("move_base", MoveBaseAction)
@@ -22,6 +34,13 @@ class MoveTB():
             rospy.spin()
 
     def assign_goal(self, pose, orientation):
+        '''
+        Assigns a pose and orientation to a MoveBaseGoal object.
+        pose: Cartiesian pose list [x, y, z]
+        orientation: Quaternion rotation list [x, y ,z ,w]
+        returns: MoveBaseGoal object
+        '''
+
         # Create MoveBaseGoal object from input
         goal_pose = MoveBaseGoal()
         goal_pose.target_pose.header.frame_id = 'map'
@@ -36,6 +55,11 @@ class MoveTB():
         return goal_pose
 
     def move_tb(self, req):
+        '''
+        Handler for move_tb service. Calls SimpleAction service to move tb to goal position.
+        req: Service request, a Move object.
+        returns: Service response.
+        '''
 
         # Assign the turtlebot's goal
         tb_goal = self.assign_goal(req.final_pose, req.final_orientation)
@@ -46,6 +70,17 @@ class MoveTB():
             return True, "Turtlebot successfully navigated to goal position"
         else:
             return False, "Turtlebot unable to navigate to goal position"
+
+    def shutdown(self):
+        '''
+        Called on shutdown of node.
+        '''
+        
+        rospy.loginfo("Stopping move_tb node")
+        
+        # Stop the turtlebot
+        self.cmd_vel.publish(Twist())
+        self.rate.sleep()
 
 if __name__ == "__main__":
     try:
