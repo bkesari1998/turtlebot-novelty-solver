ff019a3573625895a86222c96276ce320b71963c
Bharat Kesari <bkesari1998@gmail.com>
2022-07-18 15:46:53 -0400

    removed wait for open_door service

--- coordinate_navigation/scripts/action_execution.py
+++ coordinate_navigation/scripts/action_execution.py
@@ -41,8 +41,6 @@ class PlanExecutor():
         rospy.wait_for_service("dock")
         rospy.loginfo("Waiting for move service")
         rospy.wait_for_service("move")
-        rospy.loginfo("Waiting for open_door service")
-        rospy.wait_for_service("/open_door")
         rospy.loginfo("Waiting for amcl")
         rospy.wait_for_message("/amcl_pose", PoseWithCovarianceStamped, timeout=10)
 
