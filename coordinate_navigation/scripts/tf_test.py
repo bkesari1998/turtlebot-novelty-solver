52cc5d9b660bac5370a57a195d168cf7000a8347
Bharat Kesari <bkesari1998@gmail.com>
2022-07-22 13:59:34 -0400

    Test of tf2

--- coordinate_navigation/scripts/tf_test.py
+++ coordinate_navigation/scripts/tf_test.py
@@ -1,47 +1,15 @@
-#!/usr/bin/env python
-
+#!/usr/bin/env python3
 import rospy
+from geometry_msgs.msg import PoseStamped, TransformStamped
 import tf2_ros
-import geometry_msgs.msg
-from apriltag_ros.msg import AprilTagDetectionArray
+import tf2_geometry_msgs.tf2_geometry_msgs
 
-def transform_to_tag_frame(camera_frame_pose):
+one = PoseStamped()
 
-    tag_frame_pose = geometry_msgs.msg.PoseWithCovarianceStamped()
+trans = TransformStamped()
+trans.transform.translation.x = 1
 
-    # Header
-    tag_frame_pose.header.stamp = rospy.Time.now()
-    tag_frame_pose.header.frame_id = 'map'
+print(one)
 
-    # Position
-    tag_frame_pose.pose.pose.position.x = -camera_frame_pose.pose.pose.position.x
-    tag_frame_pose.pose.pose.position.y = 0
-    tag_frame_pose.pose.pose.position.z = camera_frame_pose.pose.pose.position.z
-
-    # Orientation
-    tag_frame_pose.pose.pose.orientation.w = 1
-    tag_frame_pose.pose.pose.orientation.x = 0
-    tag_frame_pose.pose.pose.orientation.y = 0
-    tag_frame_pose.pose.pose.orientation.z = 0
-
-    return tag_frame_pose
-
-
-
-
-if __name__ == '__main__':
-
-    rospy.init_node("tf_test")
-
-    tfBuffer = tf2_ros.Buffer()
-    listener = tf2_ros.TransformListener(tfBuffer)
-
-    detections_array = rospy.wait_for_message("/tag_detections", AprilTagDetectionArray, timeout=10)
-    tag = detections_array.detections[0]
-    tag_frame = "at%d" % tag.id[0]
-    
-    camera_frame_pose = transform_to_tag_frame(tag.pose)
-
-    trans = tfBuffer.transform(camera_frame_pose, tag_frame, new_type=geometry_msgs.msg.PoseWithCovarianceStamped)
-
-    print(trans)
+two = tf2_geometry_msgs.do_transform_pose(one, trans)
+print(two)
