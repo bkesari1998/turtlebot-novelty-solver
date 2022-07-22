f7b5e276e9c8b015527769619b2b9339b4a1dbe8
Bharat Kesari <bkesari1998@gmail.com>
2022-07-22 13:33:02 -0400

    tf_test

---
+++ coordinate_navigation/scripts/tf_test.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import rospy
+import tf2_ros
+import geometry_msgs.msg
+
+def transform_to_tag_frame(camera_frame_pose):
+
+    tag_frame_pose = geometry_msgs.msg.PoseWithCovarianceStamped()
+
+    # Header
+    tag_frame_pose.header.stamp = rospy.Time.now()
+    tag_frame_pose.pose.header.frame_id = 'map'
+
+    # Position
+    tag_frame_pose.pose.pose.position.x = -camera_frame_pose.pose.pose.position.x
+    tag_frame_pose.pose.pose.position.y = 0
+    tag_frame_pose.pose.pose.position.z = camera_frame_pose.pose.pose.position.z
+
+    # Orientation
+    tag_frame_pose.pose.pose.orientation.w = 1
+    tag_frame_pose.pose.pose.orientation.x = 0
+    tag_frame_pose.pose.pose.orientation.y = 0
+    tag_frame_pose.pose.pose.orientation.z = 0
+
+    return tag_frame_pose
+
+
+
+
+if __name__ == '__main__':
+
+    rospy.init_node("tf_test")
+
+    tfBuffer = tf2_ros.Buffer()
+    listener = tf2_ros.TransformListener(tfBuffer)
+
+    detections_array = rospy.wait_for_message("/tag_detections", timeout=10)
+    tag = detections_array.detections[0]
+    tag_frame = "at%d" % tag.id[0]
+    
+    camera_frame_pose = transform_to_tag_frame(tag.pose)
+
+    print(tfBuffer.transform(camera_frame_pose, tag_frame, new_type=geometry_msgs.msg.PoseWithCovarianceStamped))
