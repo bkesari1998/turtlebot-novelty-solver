bf7d8ecb84d89c84b80f4a64358930a88adbf498
Bharat Kesari <bkesari1998@gmail.com>
2022-08-13 14:41:59 -0400

    Test for read plan

--- coordinate_navigation/scripts/manager.py
+++ coordinate_navigation/scripts/manager.py
@@ -12,6 +12,27 @@ class Manager(object):
         rospy.wait_for_service("/action_executor")
         rospy.loginfo("action_executor service active")
 
+        rospy.loginfo(self.read_plan("/home/mulip-zeus/catkin_ws/src/coffee-bot/pddls/problem_2_0_1.plan"))
+
         self.action_executor_client = rospy.ServiceProxy("action_executor", Action)
 
-        self.plan_count = 0
+    def read_plan(self, plan_file_path):
+        f = open(plan_file_path, "r")
+
+        # Get lines of file
+        lines = f.readlines()
+
+        # Only get plan lines
+        plan_lines = lines[4: -2]
+
+        # Remove timestamp from plan lines
+        for i in range(len(plan_lines)):
+
+            plan_lines[i] = plan_lines[i][plan_lines[i].index("("):]
+
+        # Return plan, with each action split by space char
+        plan = []
+        for line in plan_lines:
+            plan.append(line.split(" "))
+
+        return plan
