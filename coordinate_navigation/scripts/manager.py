21f63a82507b4641352e74168d02d6cb75905170
Bharat Kesari <bkesari1998@gmail.com>
2022-08-13 16:59:59 -0400

    Made path a variable

--- coordinate_navigation/scripts/manager.py
+++ coordinate_navigation/scripts/manager.py
@@ -9,13 +9,18 @@ class Manager(object):
 
         rospy.init_node("manager")
 
-        # rospy.wait_for_service("/action_executor")
-        # rospy.loginfo("action_executor service active")
+        rospy.wait_for_service("/action_executor")
+        rospy.loginfo("action_executor service active")
 
-        plan = self.read_plan("/home/mulip/catkin_ws/src/coffee-bot/pddls/problem_2_0_2.plan")
+        self.plan_file_path = "/home/mulip/catkin_ws/src/coffee-bot/pddls/problem_2_0_1.plan" # maybe make param?
+        self.goal_state = "facing desk_1"
+
+        plan = self.read_plan(self.plan_file_path)
 
         self.action_executor_client = rospy.ServiceProxy("action_executor", Action)
 
+        # Go until goal state reached
+        
         for action in plan:
             res = self.action_executor_client(action)
 
